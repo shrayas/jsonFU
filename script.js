1ed64097dff463899b94b67e4ad8768437b263e4
Shrayas <shrayasr@gmail.com>
2013-08-10 00:06:47 +0530

    Initial worksing version of flattner + gitignore
    * gitignore added, ignoring vim and osx shiz
    * initial working version, now flattens any given json

--- script.js
+++ script.js
@@ -2,8 +2,34 @@ $(function(){
 
   $.get("data.json",function(json){
 
-    
+    console.log(json);
+
+    console.log(parse("",json));
 
   });
+
+  function parse (prefix, obj) {
+
+    if (prefix != "")
+      prefix = prefix + "/";
+
+    var returnObj = {};
+
+    var keys = Object.keys(obj);
+
+    for (keyIndex in keys) {
+
+      var key = keys[keyIndex];
+      var val = obj[key];
+
+      if (typeof val == "object")
+        jQuery.extend(returnObj,parse(prefix + key,val));
+      else 
+        returnObj[prefix + key] = val;
+    }
+
+    return returnObj;
+
+  }
 
 });
