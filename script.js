4e5bcb01eaf74238e87ed56591ba107d0ca872f1
Shrayas <shrayasr@gmail.com>
2013-08-10 01:13:32 +0530

    Finishing inflating implementation, works!
    * Adding a initial *working* implementation of both flat and inflate
      algos.
    
    TODO documentation, code cleanup

--- script.js
+++ script.js
@@ -2,11 +2,43 @@ $(function(){
 
   $.get("data.json",function(json){
 
-    console.log(json);
-
-    console.log(parse("",json));
+    console.log("I/P: " + JSON.stringify(json));
+    var flat = parse("",json);
+    console.log("FLAT: " + JSON.stringify(flat));
+    var unflat = unParse(flat);
+    console.log("INFLATED: " + JSON.stringify(unflat));
 
   });
+
+  function fxn(key,val) {
+
+    var returnObj = {};
+
+    var hasSlash = key.indexOf("/");
+
+    if (hasSlash == -1) {
+      returnObj[key]=val;
+    }
+
+    else
+    {
+      var effectiveKey = key.substring(0,key.indexOf("/"));
+      var remaining = key.substring(key.indexOf("/")+1);
+
+      var hasMoreKeys = remaining.indexOf("/");
+      var intermediate = {}
+
+      if (hasMoreKeys != -1) {
+        returnObj[effectiveKey] = fxn(remaining,val);
+      }
+      else {
+       intermediate[remaining] = val;
+       returnObj[effectiveKey] = intermediate;
+      }
+    }
+
+    return returnObj;
+  }
 
   function parse (prefix, obj) {
 
@@ -26,6 +58,27 @@ $(function(){
         jQuery.extend(returnObj,parse(prefix + key,val));
       else 
         returnObj[prefix + key] = val;
+    }
+
+    return returnObj;
+
+  }
+
+  function unParse(obj) {
+
+    var returnObj = {};
+
+    var keys = Object.keys(obj);
+
+    for (keyIndex in keys) {
+
+      var key = keys[keyIndex];
+      var val = obj[key];
+
+      var iobj = fxn(key,val);
+
+      $.extend(true,returnObj,iobj);
+
     }
 
     return returnObj;
